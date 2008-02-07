55d4ec5881ed4ef7312fec1e439ca9e0227f6e16
Olav Bjorkoy <olav@bjorkoy.com>
2008-02-07 19:34:50 +0000

    Added some basic comments to validator.

--- lib/validate.rb
+++ lib/validate.rb
@@ -1,7 +1,15 @@
 #!/usr/bin/env ruby
-
 require 'blueprint/blueprint'
 require 'blueprint/validate/validator'
+
+# This script will validate the core Blueprint files. 
+# 
+# The files are not completely valid. This has to do 
+# with a small number of CSS hacks needed to ensure 
+# consistent rendering across browsers.
+#
+# To add your own CSS files for validation, see
+# /lib/blueprint/validate/validator.rb
 
 v = Validator.new
 v.validate
