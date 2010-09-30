2d9ac52bd4386c4708fee1c1b86a40d9302760f0
Christian Montoya <sirokai@gmail.com>
2010-09-30 06:22:57 -0400

    Updating documentation for version 1.0

--- lib/blueprint/compressor.rb
+++ lib/blueprint/compressor.rb
@@ -272,7 +272,7 @@ module Blueprint
 %(/* -----------------------------------------------------------------------
 
 
- Blueprint CSS Framework 0.9
+ Blueprint CSS Framework 1.0
  http://blueprintcss.org
 
    * Copyright (c) 2007-Present. See LICENSE for more info.
