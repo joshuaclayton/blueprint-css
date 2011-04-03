9bd92dd2bc10873812e3b1064db0123b648e460e
Owen Jones and Josh Clayton <owengalenjones+joshua.clayton@gmail.com>
2011-04-03 16:20:39 -0400

    Fixing hr bottom margin by setting to 17px
    Deleting debug.zip
    
    Merging updates from Eric Meyer's latest reset.css
    
    Adding explicit font color to input & textarea
    
    Added constant FONT_SIZE defaults to blueprint standard of 12
    
    Added symbol :font_size
    
    If compressor.rb sees custom layout and the css_source_file is typography.css it runs: custom_layout.generate_typography_css
    
    Modified the compressor output
    
    Added attr_writer :font_size
    
    define font_size to return as a float or Blueprint default (12)
    
    defined generate_typography_css using the same syntax as generate_grid_css
    
    Initial commit, erb file of typography
    
    Only change is to set the (font_size / 16) * 100
    12 / 16 * 100 = 75
    
    Adjusts height based on either passing in a baseline_height, automatically computed from passing in font_size or from the Blueprint default (12)
    
    Baseline is font size * 1.5
    
    Baseline will always be 1.5 * font_size
    
    Added setting font_size example
    
    adjusted hr margin based on baseline (1.5 * font_size) - 1 (hr height)
    
    hr margins are to_i

--- lib/blueprint/compressor.rb
+++ lib/blueprint/compressor.rb
@@ -66,6 +66,8 @@ module Blueprint
               "Set a new gutter width (in pixels) for the output grid") {|gw| self.custom_layout.gutter_width = gw }
         o.on( "--column_count=COLUMN_COUNT", Integer,
               "Set a new column count for the output grid") {|cc| self.custom_layout.column_count = cc }
+        o.on( "--font_size=FONT_SIZE", Integer,
+              "Set a new font size for the output grid") {|fs| self.custom_layout.font_size = fs }
         #o.on("-v", "--verbose", "Turn on verbose output.") { |$verbose| }
         o.on("-h", "--help", "Show this help message.") { puts o; exit }
       end
@@ -94,11 +96,12 @@ module Blueprint
         self.plugins =          project["plugins"]          || []
 
         if (layout = project["custom_layout"])
-          self.custom_layout = CustomLayout.new(:column_count =>  layout["column_count"],
-                                                :column_width =>  layout["column_width"],
-                                                :gutter_width =>  layout["gutter_width"],
+          self.custom_layout = CustomLayout.new(:column_count  => layout["column_count"],
+                                                :column_width  => layout["column_width"],
+                                                :gutter_width  => layout["gutter_width"],
                                                 :input_padding => layout["input_padding"],
-                                                :input_border =>  layout["input_border"])
+                                                :input_border  => layout["input_border"],
+                                                :font_size     => layout["font_size"])
         end
         @loaded_from_settings = true
       end
@@ -120,6 +123,8 @@ module Blueprint
 
           source_options = if self.custom_layout && css_source_file == "grid.css"
             self.custom_layout.generate_grid_css
+          elsif self.custom_layout && css_source_file == "typography.css"
+            self.custom_layout.generate_typography_css
           else
             File.path_to_string File.join(Blueprint::SOURCE_PATH, css_source_file)
           end
@@ -145,7 +150,8 @@ module Blueprint
       #attempt to generate a grid.png file
       if (grid_builder = GridBuilder.new(:column_width => self.custom_layout.column_width,
                                          :gutter_width => self.custom_layout.gutter_width,
-                                         :output_path => File.join(self.destination_path, "src")))
+                                         :font_size    => self.custom_layout.font_size,
+                                         :output_path  => File.join(self.destination_path, "src")))
         grid_builder.generate!
       end
     end
@@ -254,6 +260,7 @@ module Blueprint
       puts "  **     - Column Width: #{self.custom_layout.column_width}px"
       puts "  **     - Gutter Width: #{self.custom_layout.gutter_width}px"
       puts "  **     - Total Width : #{self.custom_layout.page_width}px"
+      puts "  **     - Font Size   : #{self.custom_layout.font_size}px"
       puts "  **"
       puts "  #{"*" * 100}"
     end
